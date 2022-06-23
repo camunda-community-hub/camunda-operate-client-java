2d0b84fb0f6856701aca6a13fdc42490c1c17f0a
Christophe Dame <christophe.dame@camunda.com>
2022-06-23 09:59:23 +0200

    make code java 8 compliant

--- src/main/java/io/camunda/operate/CamundaOperateConstants.java
+++ src/main/java/io/camunda/operate/CamundaOperateConstants.java
@@ -1,6 +1,9 @@
 package io.camunda.operate;
 
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import io.camunda.operate.dto.FlownodeInstance;
 import io.camunda.operate.dto.Incident;
@@ -10,10 +13,18 @@ import io.camunda.operate.dto.Variable;
 
 public class CamundaOperateConstants {
 
-    public static Map<Class<?>, String> OBJECT_APIS = Map.of(ProcessDefinition.class, "/v1/process-definitions", 
+    public static Map<Class<?>, String> OBJECT_APIS = toMap(ProcessDefinition.class, "/v1/process-definitions", 
                 ProcessInstance.class, "/v1/process-instances",
                 FlownodeInstance.class, "/v1/flownode-instances",
                 Incident.class, "/v1/incidents",
                 Variable.class, "/v1/variables");
     
+    private static Map<Class<?>, String> toMap(Object... array) {
+        AbstractMap.SimpleEntry<Class<?>, String>[] entryArray = new AbstractMap.SimpleEntry[array.length/2];
+        for(int i=0;i<entryArray.length;i++) {
+            entryArray[i]= new  AbstractMap.SimpleEntry<Class<?>, String>((Class<?>)array[i*2], (String) array[i*2+1]);
+        }
+        return Stream.of(entryArray)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 }
