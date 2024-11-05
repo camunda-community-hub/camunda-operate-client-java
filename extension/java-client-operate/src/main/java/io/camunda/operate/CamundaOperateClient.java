68b46fca1f97152234c07a723db28d58b455d107
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-11-05 20:20:59 +0100

    moved default implementations to interface

--- extension/java-client-operate/src/main/java/io/camunda/operate/CamundaOperateClient.java
+++ extension/java-client-operate/src/main/java/io/camunda/operate/CamundaOperateClient.java
@@ -13,51 +13,83 @@ import io.camunda.operate.model.ProcessInstance;
 import io.camunda.operate.model.SearchResult;
 import io.camunda.operate.model.Variable;
 import io.camunda.operate.search.SearchQuery;
+import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 public interface CamundaOperateClient {
   ProcessDefinition getProcessDefinition(Long key) throws OperateException;
 
-  BpmnModelInstance getProcessDefinitionModel(Long key) throws OperateException;
+  default BpmnModelInstance getProcessDefinitionModel(Long key) throws OperateException {
+    String xml = getProcessDefinitionXml(key);
+    try {
+      InputStream processInputStream = new ByteArrayInputStream(xml.getBytes());
+      return Bpmn.readModelFromStream(processInputStream);
+    } catch (Exception e) {
+      throw new OperateException(e);
+    }
+  }
 
   String getProcessDefinitionXml(Long key) throws OperateException;
 
-  List<ProcessDefinition> searchProcessDefinitions(SearchQuery query) throws OperateException;
+  default List<ProcessDefinition> searchProcessDefinitions(SearchQuery query)
+      throws OperateException {
+    return searchProcessDefinitionResults(query).getItems();
+  }
 
   SearchResult<ProcessDefinition> searchProcessDefinitionResults(SearchQuery query)
       throws OperateException;
 
-  List<DecisionDefinition> searchDecisionDefinitions(SearchQuery query) throws OperateException;
+  default List<DecisionDefinition> searchDecisionDefinitions(SearchQuery query)
+      throws OperateException {
+    return searchDecisionDefinitionResults(query).getItems();
+  }
 
   SearchResult<DecisionDefinition> searchDecisionDefinitionResults(SearchQuery query)
       throws OperateException;
 
-  List<DecisionInstance> searchDecisionInstances(SearchQuery query) throws OperateException;
+  default List<DecisionInstance> searchDecisionInstances(SearchQuery query)
+      throws OperateException {
+    return searchDecisionInstanceResults(query).getItems();
+  }
 
   SearchResult<DecisionInstance> searchDecisionInstanceResults(SearchQuery query)
       throws OperateException;
 
-  List<FlowNodeInstance> searchFlowNodeInstances(SearchQuery query) throws OperateException;
+  default List<FlowNodeInstance> searchFlowNodeInstances(SearchQuery query)
+      throws OperateException {
+    return searchFlowNodeInstanceResults(query).getItems();
+  }
 
   SearchResult<FlowNodeInstance> searchFlowNodeInstanceResults(SearchQuery query)
       throws OperateException;
 
-  List<Variable> searchVariables(SearchQuery query) throws OperateException;
+  default List<Variable> searchVariables(SearchQuery query) throws OperateException {
+    return searchVariableResults(query).getItems();
+  }
 
   SearchResult<Variable> searchVariableResults(SearchQuery query) throws OperateException;
 
-  List<ProcessInstance> searchProcessInstances(SearchQuery query) throws OperateException;
+  default List<ProcessInstance> searchProcessInstances(SearchQuery query) throws OperateException {
+    return searchProcessInstanceResults(query).getItems();
+  }
 
   SearchResult<ProcessInstance> searchProcessInstanceResults(SearchQuery query)
       throws OperateException;
 
-  List<DecisionRequirements> searchDecisionRequirements(SearchQuery query) throws OperateException;
+  default List<DecisionRequirements> searchDecisionRequirements(SearchQuery query)
+      throws OperateException {
+    return searchDecisionRequirementsResults(query).getItems();
+  }
 
   SearchResult<DecisionRequirements> searchDecisionRequirementsResults(SearchQuery query)
       throws OperateException;
 
-  List<Incident> searchIncidents(SearchQuery query) throws OperateException;
+  default List<Incident> searchIncidents(SearchQuery query) throws OperateException {
+    return searchIncidentResults(query).getItems();
+  }
 
   SearchResult<Incident> searchIncidentResults(SearchQuery query) throws OperateException;
 
