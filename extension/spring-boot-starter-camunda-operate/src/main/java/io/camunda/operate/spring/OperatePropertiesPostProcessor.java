73c3354a10d328f9a6f076468a43d16ec9f1440a
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-11-05 20:03:33 +0100

    created a yaml for the operate profile

--- extension/spring-boot-starter-camunda-operate/src/main/java/io/camunda/operate/spring/OperatePropertiesPostProcessor.java
+++ extension/spring-boot-starter-camunda-operate/src/main/java/io/camunda/operate/spring/OperatePropertiesPostProcessor.java
@@ -54,6 +54,9 @@ public class OperatePropertiesPostProcessor implements EnvironmentPostProcessor 
       case simple -> {
         return "simple.yaml";
       }
+      case v2 -> {
+        return "v2.yaml";
+      }
     }
     throw new IllegalStateException("Unknown client mode " + clientMode);
   }
