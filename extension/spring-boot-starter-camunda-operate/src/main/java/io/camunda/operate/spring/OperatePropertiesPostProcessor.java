aa36abf688a4be3c15271d37964394df32a7f591
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-06-25 09:57:42 +0200

    add more fine-grained config for client assertion keystore (#258)
    * add more fine-grained config for client assertion keystore
    
    * added docs
    
    * added undeclared deps
    
    (cherry picked from commit 750d891c5fdaa921e81d7b231eb9dbeefe4cea26)

--- extension/spring-boot-starter-camunda-operate/src/main/java/io/camunda/operate/spring/OperatePropertiesPostProcessor.java
+++ extension/spring-boot-starter-camunda-operate/src/main/java/io/camunda/operate/spring/OperatePropertiesPostProcessor.java
@@ -1,15 +1,24 @@
 package io.camunda.operate.spring;
 
 import io.camunda.operate.spring.OperateClientConfigurationProperties.Profile;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import org.apache.commons.logging.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.boot.logging.DeferredLogFactory;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 
 public class OperatePropertiesPostProcessor implements EnvironmentPostProcessor {
+  private final Log log;
+
+  public OperatePropertiesPostProcessor(DeferredLogFactory deferredLogFactory) {
+    log = deferredLogFactory.getLog(OperatePropertiesPostProcessor.class);
+  }
 
   @Override
   public void postProcessEnvironment(
@@ -17,7 +26,10 @@ public class OperatePropertiesPostProcessor implements EnvironmentPostProcessor 
     try {
       Profile profile = environment.getProperty("operate.client.profile", Profile.class);
       if (profile == null) {
-        return;
+        profile = detectProfile(environment);
+        if (profile == null) {
+          return;
+        }
       }
       YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
       String propertiesFile = "operate-profiles/" + determinePropertiesFile(profile);
@@ -44,5 +56,40 @@ public class OperatePropertiesPostProcessor implements EnvironmentPostProcessor 
       }
     }
     throw new IllegalStateException("Unknown client mode " + clientMode);
+  }
+
+  private Profile detectProfile(ConfigurableEnvironment environment) {
+    // cluster id is set -> always saas
+    if (environment.getProperty("operate.client.cluster-id") != null) {
+      log.debug("Detected 'operate.client.profile'='saas' based on 'operate.client.cluster-id'");
+      return Profile.saas;
+    }
+    // here, we can try to distinguish between simple and oidc
+    Set<Profile> potentialProfiles = new HashSet<>();
+    if (environment.getProperty("operate.client.username") != null) {
+      log.debug("Detected 'operate.client.profile'='simple' based on 'operate.client.username'");
+      potentialProfiles.add(Profile.simple);
+    }
+    if (environment.getProperty("operate.client.password") != null) {
+      log.debug("Detected 'operate.client.profile'='simple' based on 'operate.client.password'");
+      potentialProfiles.add(Profile.simple);
+    }
+    if (environment.getProperty("operate.client.client-id") != null) {
+      log.debug("Detected 'operate.client.profile'='oidc' based on 'operate.client.client-id'");
+      potentialProfiles.add(Profile.oidc);
+    }
+    if (environment.getProperty("operate.client.client-secret") != null) {
+      log.debug("Detected 'operate.client.profile'='oidc' based on 'operate.client.client-secret'");
+      potentialProfiles.add(Profile.oidc);
+    }
+    if (potentialProfiles.isEmpty()) {
+      log.debug("No 'operate.client.profile' could be detected");
+      return null;
+    }
+    if (potentialProfiles.size() > 1) {
+      log.warn(
+          "Multiple implicit values for 'operate.client.profile' detected: " + potentialProfiles);
+    }
+    return potentialProfiles.iterator().next();
   }
 }
